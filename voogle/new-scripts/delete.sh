dc272bc7c00ec4117ac39edbd268a953f624a142
Voncloft <voncloft@gmail.com>
2022-02-04 16:23:04 -0500

    Minor Updates

--- voogle/new-scripts/delete.sh
+++ voogle/new-scripts/delete.sh
@@ -3,6 +3,7 @@ mysql_path_pre="http://voncloft.com:90/all"
 final_mysql_location=$mysql_path_pre$1
 
 command="delete from Filenames where location ='$final_mysql_location'" 
+echo $command
 mysql -u${username} -p${password} --database="Intranet" --execute="$command;"
 echo "$1 deleted from database"
-#echo $command
+echo "$command" >> /var/log/old/search_engine_events
