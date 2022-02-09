c7440c5cc6d03ae28845c7c92759c1b8a998de10
Voncloft <voncloft@gmail.com>
2022-02-09 15:55:11 -0500

    Minor Updates

--- voogle/new-scripts/update.sh
+++ voogle/new-scripts/update.sh
@@ -5,5 +5,5 @@ final_mysql_location=$mysql_path_pre$1
 command="update Filenames set location='${mysql_path_pre}$2' where location = '${mysql_path_pre}$1'" 
 echo $command
 mysql -u${username} -p${password} --database="Intranet" --execute="$command;"
-echo "$1 updated final location"
+echo "$1 updated final location" >>  /var/log/old/search_engine_events
 echo "$command" >> /var/log/old/search_engine_events
